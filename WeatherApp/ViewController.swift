3a39e71dc64c69d57a749d1fe3e1abc2ee285c71
Rida <rida.toukrichte250@gmail.com>
2018-12-21 21:31:04 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Rida TOUKRICHTE on 21/12/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
